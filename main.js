91f447edf2ba88a7fb8cb7893f5ebb07840f60a4
Michael Monaghan <Nostromos@users.noreply.github.com>
2023-07-24 14:53:08 -0400

    Completed factory function with mutate() and compareDNA(). Updated readme to reflect progress.

--- main.js
+++ main.js
@@ -2,22 +2,44 @@
 const returnRandBase = () => {
     const dnaBases = ['A', 'T', 'C', 'G']
     return dnaBases[Math.floor(Math.random() * 4)] 
-  }
+}
   
-  // Returns a random single strand of DNA containing 15 bases
-  const mockUpStrand = () => {
+// Returns a random single strand of DNA containing 15 bases
+const mockUpStrand = () => {
     const newStrand = []
     for (let i = 0; i < 15; i++) {
-      newStrand.push(returnRandBase())
+        newStrand.push(returnRandBase())
     }
-    return newStrand
-  }
+    return newStrand;
+}
   
+const pAequorFactory = (uniqueNumber, dnaArray) => {
+    return {
+        specieNum: uniqueNumber,
+        dna: dnaArray,
+        mutate () {
+            const randBaseNum = Math.floor(Math.random() * this.dna.length);
+            let baseToMutate = this.dna[randBaseNum];
+            while (baseToMutate === this.dna[randBaseNum]) {
+                baseToMutate = returnRandBase();
+            }
+            this._dna[randBaseNum] = baseToMutate;
+        },
+        compareDNA (otherAeqObj) {
+            let commonCount = 0;
+            otherAeqObj.dna.forEach((base, i) => {
+                if (base === this.dna[i]) {
+                    commonCount++;
+                }
+            })
+            const percentCommon = Math.trunc((commonCount / 15) * 100);
+            console.log(`Specie ${this.specieNum} and Specie ${otherAeqObj.specieNum} have ${percentCommon}% DNA in common.`)
+        }
+    }
+}
   
-  
-  
-  
-  
-  
-  
+let specie1 = pAequorFactory(1, mockUpStrand());
+let specie2 = pAequorFactory(2, mockUpStrand());
+
+specie2.compareDNA(specie1);
   
